f619e6925557338cdcffee3813b266378d82dacf
Warren Yao <warrenyca905@gmail.com>
2019-08-11 03:52:03 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -34,5 +34,5 @@ class InteractiveRecord
   end
   
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert)"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})"
 end
