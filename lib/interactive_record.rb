7234816b7a7afb68d4c9deb04e3769405efd6808
Warren Yao <warrenyca905@gmail.com>
2019-08-11 04:02:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,4 +38,8 @@ class InteractiveRecord
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
+  
+  def self.find_by_name(name)
+    sql = 
+  end
 end
